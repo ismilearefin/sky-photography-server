fd6790372c74602e1b47e709253e18e23abb429e
ismilearefin <arefinismile@gmail.com>
2022-11-08 00:21:22 +0600

    server setup and connect with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.y3njyog.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run (){
+
+
+
+
+
+    
+}
+run().catch((err) => console.log(err))
 
 
 
